479561232dd51ffb100e0423aeeaedbc0acf4bbd
Imantha-Kodikara <imantha.l.kodikara@gmail.com>
2025-05-28 12:09:17 +0530

    dashboard UI and rooms management UI created

--- the-kings-hotel-reservation-system/src/main/java/controller/DashboardFormController.java
+++ the-kings-hotel-reservation-system/src/main/java/controller/DashboardFormController.java
@@ -1,4 +1,67 @@
 package controller;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class DashboardFormController {
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Button btnRoom;
+
+    @FXML
+    public void initialize(){
+        LocalDate today = LocalDate.now();
+        lblDate.setText(today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e ->{
+            LocalTime currentTime = LocalTime.now();
+            lblTime.setText(currentTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Timeline.INDEFINITE);
+        clock.play();
+    }
+
+    @FXML
+    public void btnRoomManagementOnClick(ActionEvent actionEvent) {
+      Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/rooms_management_form_view.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void btnReservationManagementOnClick(ActionEvent actionEvent) {
+    }
+
+    public void btnCustomerManagementOnClick(ActionEvent actionEvent) {
+    }
+
+    public void btnBillingOnClick(ActionEvent actionEvent) {
+    }
+
+    public void btnReportOnClick(ActionEvent actionEvent) {
+    }
+
+    public void btnCheckInOnClick(ActionEvent actionEvent) {
+    }
 }
---
+++ the-kings-hotel-reservation-system/src/main/java/controller/RoomsManagementFormController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class RoomsManagementFormController {
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    void btnAddRoomOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteRoomOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateRoomOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewRoomOnClick(ActionEvent event) {
+
+    }
+
+}
